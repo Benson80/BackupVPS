c5933f0e54edef6215c2aa860bba394afeee5e4a
Benson80 <81787444+Benson80@users.noreply.github.com>
2024-02-17 13:24:09 +0800

    Update backupVPS.sh

--- backupVPS.sh
+++ backupVPS.sh
@@ -1,20 +1,16 @@
 #!/bin/bash
 
-# 设置临时目录
-temp_dir="/tmp"
-
 # 备份文件名
 backup_file="backup_$(date +'%Y%m%d_%H%M%S').zip"
 
 # 同步 rclone 挂载的路径到本地
-rclone sync tianyi:甲骨文VPS备份 "$temp_dir"
+rclone sync tianyi:甲骨文VPS备份 "$backup_file"
 
 # 备份文件到 zip 压缩包
-zip -r "$temp_dir/$backup_file" "$temp_dir/tianyicloud"
+zip -r "$backup_file" "$backup_file"
 
 # 备份完成后，使用 rclone 同步到天翼云盘
-rclone sync "$temp_dir/$backup_file" tianyi:甲骨文VPS备份
+rclone sync "$backup_file" tianyi:甲骨文VPS备份
 
-# 清理本地备份文件
-rm "$temp_dir/$backup_file"
-rm -rf "$temp_dir/tianyicloud"
+# 删除本地备份文件
+rm "$backup_file"
