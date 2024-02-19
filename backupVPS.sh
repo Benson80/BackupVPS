eb0450a2a7e2781efe95b9500f8c24f7d737d047
Benson80 <81787444+Benson80@users.noreply.github.com>
2024-02-19 14:24:55 +0800

    Update backupVPS.sh

--- backupVPS.sh
+++ backupVPS.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # 设置备份目录和备份文件名
-backup_dir="/etc /home /var /root /boot"
-backup_file="backup_$(date +'%Y%m%d_%H%M%S').zip"
+backup_dirs="/etc /home /var /root /boot"
+backup_file="backup_$(date +'%Y%m%d_%H%M%S').tar.gz"
 
-# 备份文件到 zip 压缩包
-zip -r "$backup_file" $backup_dir
+# 备份文件到 tar 压缩包
+tar czvf "$backup_file" $backup_dirs
 
 # 备份完成后，使用 rclone 同步到天翼云盘
 #rclone sync "$backup_file" tianyi:甲骨文VPS备份
