c63271dc31baa852852bb92af336bf57f8d83251
Benson80 <81787444+Benson80@users.noreply.github.com>
2024-02-19 14:10:47 +0800

    Update backupVPS.sh

--- backupVPS.sh
+++ backupVPS.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
 # 设置备份目录和备份文件名
-backup_dirs=("/etc" "/home" "/usr/local" "/root")
-backup_time=$(date +'%Y%m%d_%H%M%S')
+backup_dirs=("/etc" "/home" "/var" "/root" "/boot")
+backup_file="backup_$(date +'%Y%m%d_%H%M%S').zip"
 
-# 错误处理函数
-handle_error() {
-    echo "Error occurred at line $1"
-    exit 1
-}
-trap 'handle_error $LINENO' ERR
-
-# 备份每个目录到单独的 zip 压缩包
-for dir in "${backup_dirs[@]}"; do
-    backup_file="backup_${dir##*/}_${backup_time}.zip"
-    zip -r "$backup_file" "$dir"
-done
+# 备份文件到 zip 压缩包
+zip -r "$backup_file" $backup_dir
 
 # 备份完成后，使用 rclone 同步到天翼云盘
-#rclone sync "$backup_file" tianyi:甲骨文VPS备份
+rclone sync "$backup_file" tianyi:甲骨文VPS备份
 
 # 删除本地备份文件
 #rm "$backup_file"
