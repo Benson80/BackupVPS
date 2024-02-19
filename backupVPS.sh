ab7efaf49cb41264eb18b54d9c9f792175f3cb67
Benson80 <81787444+Benson80@users.noreply.github.com>
2024-02-19 14:21:35 +0800

    Update backupVPS.sh

--- backupVPS.sh
+++ backupVPS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置备份目录和备份文件名
-backup_dirs="/etc /home /var /root /boot"
+backup_dir="/etc /home /var /root /boot"
 backup_file="backup_$(date +'%Y%m%d_%H%M%S').zip"
 
 # 备份文件到 zip 压缩包
